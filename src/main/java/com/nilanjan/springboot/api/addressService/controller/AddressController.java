7d5821ad13eade600a08d35146ad46456cc7a1a6
nilan <nilan@host.docker.internal>
2025-08-02 16:45:34 +0530

    Add logging with custom configuration

--- src/main/java/com/nilanjan/springboot/api/addressService/controller/AddressController.java
+++ src/main/java/com/nilanjan/springboot/api/addressService/controller/AddressController.java
@@ -1,5 +1,6 @@
 package com.nilanjan.springboot.api.addressService.controller;
 
+import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,13 +11,19 @@ import com.nilanjan.springboot.api.addressService.service.AddressService;
 @RestController
 @RequestMapping("/v1")
 public class AddressController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(AddressController.class);
+	
 	@Autowired
 	private AddressService service;
 	
 	@GetMapping("/studentaddress/{id}")
-	public ResponseEntity<AddressResponse> getAddressByEmployeeId(@PathVariable int id){
+	public ResponseEntity<AddressResponse> findAddressByStudentId(@PathVariable int id){
+		logger.debug("Entering into findAddressByStudentId.");
+		logger.debug("id: ["+id+"]");	
 		AddressResponse response = service.findAddressByStudentId(id);
+		logger.debug("rsponse: [" + response.toString() + "]");	
+		logger.debug("Going out from findAddressByStudentId.");	
 		return ResponseEntity.ok(response);
-		
 	}
 }
